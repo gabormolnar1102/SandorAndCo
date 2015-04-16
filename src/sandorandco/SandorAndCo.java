ddf20527dda58dc6a9834046aa4b44e9b34d0d16
Gábor Molnár <gabormolnar1102@gmail.com>
2015-04-16 15:40:05 +0200

    basics
    first version of the chronicles

---
+++ src/sandorandco/SandorAndCo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sandorandco;
+
+import sandorandco.views.MainView;
+
+/**
+ *
+ * @author Molnár Gábor
+ */
+public class SandorAndCo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainView mainpane = new MainView();
+        
+    }
+    
+}
